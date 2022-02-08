a18377e8faaedc7c4c2f7589b1e6019741010d24
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-08 23:29:21 +0530

    Password Validity

---
+++ PasswordValidity.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to check the validity of password input by users.
+At least 1 letter between [a-z] and 1 letter between [A-Z].
+At least 1 number between [0-9].
+At least 1 character from [$#@_^&*].
+Minimum length of 8 characters.
+Maximum length 16 characters.
+
+'''
